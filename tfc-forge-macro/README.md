7932a5d9f2760236187f5ecbdf6e14f07345bc11
Nayan Sawyer <33187059+opus-tango@users.noreply.github.com>
2026-04-02 15:02:55 -0400

    initialize macro program

---
+++ tfc-forge-macro/README.md
@@ -0,0 +1,1 @@
+This project is for setting up a macro program to automate forging in terrafirmacraft.
---
+++ tfc-forge-macro/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from tfc-forge-macro!")
+
+
+if __name__ == "__main__":
+    main()
