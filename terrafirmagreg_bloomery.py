dc5ec3c6c51a770fd99f1d1d4cf740b7b6418d33
Nayan Sawyer <33187059+opus-tango@users.noreply.github.com>
2026-04-02 15:00:31 -0400

    update bloomery script

--- terrafirmagreg_bloomery.py
+++ terrafirmagreg_bloomery.py
@@ -1,4 +1,4 @@
-num_ore = 19
+num_ore = 25
 mb_per_ore = 129
 mb_per_ingot = 144
 mb_per_coal = 144
@@ -9,4 +9,4 @@ num_ingots = int(mb / mb_per_ingot)
 num_coal = int(mb / mb_per_coal)
 lost_mb = mb % mb_per_ingot
 
-print(f"With {num_ore} ore and {num_coal} coal, you will get {num_ingots} ingots and lose {lost_mb} MB of iron.")
+print(f"With {num_ore} ore and {num_coal} coal, you will get {num_ingots} ingots and lose {lost_mb} MB of iron. Total items are {num_ore + num_coal}.")
